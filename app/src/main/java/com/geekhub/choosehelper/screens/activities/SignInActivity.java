a0b5d4850aac0f0aa4cd090544904b778c03b924
marinovskiy <alexander.marinovskiy@gmail.com>
2016-02-18 21:32:16 +0200

    add sign in/up activites

---
+++ app/src/main/java/com/geekhub/choosehelper/screens/activities/SignInActivity.java
@@ -0,0 +1,29 @@
+package com.geekhub.choosehelper.screens.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.geekhub.choosehelper.R;
+
+import butterknife.OnClick;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    @OnClick(R.id.sign_in_tv)
+    public void onClick() {
+        startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+    }
+}
---
+++ app/src/main/java/com/geekhub/choosehelper/screens/activities/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.geekhub.choosehelper.screens.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.geekhub.choosehelper.R;
+
+import butterknife.OnClick;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    @OnClick(R.id.sign_up_tv)
+    public void onClick() {
+        startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+    }
+}
