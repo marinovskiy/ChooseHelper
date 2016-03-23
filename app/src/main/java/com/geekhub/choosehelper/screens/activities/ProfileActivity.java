7316681c7b9205da38b412fd9870df270b289dfe
Andriy Mytsenko <ukrainiancat@gmail.com>
2016-03-23 17:47:14 +0200

    creating profile

--- app/src/main/java/com/geekhub/choosehelper/screens/activities/ProfileActivity.java
+++ app/src/main/java/com/geekhub/choosehelper/screens/activities/ProfileActivity.java
@@ -3,22 +3,15 @@ package com.geekhub.choosehelper.screens.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 import com.geekhub.choosehelper.R;
 import com.geekhub.choosehelper.models.db.User;
 import com.geekhub.choosehelper.utils.Prefs;
-import com.geekhub.choosehelper.utils.Utils;
 import com.geekhub.choosehelper.utils.db.DbUsersManager;
-import com.geekhub.choosehelper.utils.firebase.FirebaseConstants;
 
 import butterknife.Bind;
 
@@ -26,14 +19,14 @@ public class ProfileActivity extends BaseSignInActivity {
 
     private static final String TAG = "ProfileActivity";
 
-    @Bind(R.id.toolbar_profile)
+    @Bind(R.id.profile_toolbar)
     Toolbar mToolbar;
 
     @Bind(R.id.profile_iv_avatar)
     ImageView mIvUserAvatar;
 
-    @Bind(R.id.profile_test_image)
-    ImageView mIvTestImg;
+    @Bind(R.id.profile_tv_username)
+    TextView mUsername;
 
     @Bind(R.id.profile_tv_email)
     TextView mEmail;
@@ -44,29 +37,20 @@ public class ProfileActivity extends BaseSignInActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Get user profile info
         mUser = DbUsersManager.getUserNotAsync(Prefs.getUserId());
+
         Glide.with(getApplicationContext())
                 .load(mUser.getPhotoUrl())
                 .into(mIvUserAvatar);
         mEmail.setText(mUser.getEmail());
-        //AuthorizationUtil.getUserAsync(Prefs.getUserId());
+        mUsername.setText(mUser.getFullName());
+
+        // Init toolbar
         setupToolbar();
-        /*User user = DbUsersManager.getUserAsync(Prefs.getUserId());
-        if (user != null && user.isLoaded()) {
-            Log.i("MainActivityTest", "setupNavHeader: " + user.getId());
-            Log.i("MainActivityTest", "setupNavHeader: " + user.getFullName());
-            Log.i("MainActivityTest", "setupNavHeader: " + user.getEmail());
-            Log.i("MainActivityTest", "setupNavHeader: " + user.getPhotoUrl());
-            Log.i("MainActivityTest", "setupNavHeader: " + user.getBirthday());
-            Log.i("MainActivityTest", "setupNavHeader: " + user.getPlaceLive());
-            Log.i("MainActivityTest", "setupNavHeader: " + user.getAbout());
-        }*/
-        /*Glide
-                .with(this)
-                .load(Prefs.getUserAvatarUrl())
-                .bitmapTransform(new CropCircleTransformation(getApplicationContext()))
-                .into(mIvUserAvatar);*/
-        final String[] img = {null};
+
+        /*final String[] img = {null};
         Firebase firebase = new Firebase("https://choosehelper.firebaseio.com/img");
         firebase.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -79,23 +63,7 @@ public class ProfileActivity extends BaseSignInActivity {
             public void onCancelled(FirebaseError firebaseError) {
                 Log.i(TAG, "error: " + firebaseError);
             }
-        });
-        mIvTestImg.setImageBitmap(Utils.convertStringToBitmap(img[0]));
-        /*Glide
-                .with(this)
-                .load(Utils.convertStringToBitmap(img[0]))
-                .into(mIvTestImg);*/
-
-    }
-
-    private void setupToolbar() {
-        setSupportActionBar(mToolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(mUser.getFullName());
-            getSupportActionBar().setHomeAsUpIndicator(ContextCompat
-                    .getDrawable(getApplicationContext(), R.drawable.icon_arrow_back));
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
+        });*/
     }
 
     @Override
@@ -106,6 +74,16 @@ public class ProfileActivity extends BaseSignInActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.toolbar_title);
+            getSupportActionBar().setHomeAsUpIndicator(ContextCompat
+                    .getDrawable(getApplicationContext(), R.drawable.icon_arrow_back));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
 }
