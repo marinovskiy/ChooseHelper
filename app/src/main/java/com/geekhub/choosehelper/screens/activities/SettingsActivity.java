533dd9c456f1223a05ffab3e4250fd6c9bccf3da
Andriy Mytsenko <ukrainiancat@gmail.com>
2016-04-12 00:45:23 +0300

    test

--- app/src/main/java/com/geekhub/choosehelper/screens/activities/SettingsActivity.java
+++ app/src/main/java/com/geekhub/choosehelper/screens/activities/SettingsActivity.java
@@ -1,16 +1,20 @@
 package com.geekhub.choosehelper.screens.activities;
 
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.geekhub.choosehelper.R;
+import com.geekhub.choosehelper.dialogs.SettingsAlertDialog;
 import com.geekhub.choosehelper.models.ui.Settings;
 import com.geekhub.choosehelper.ui.adapters.SettingsAdapter;
+import com.geekhub.choosehelper.utils.Prefs;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,17 +32,23 @@ public class SettingsActivity extends BaseSignInActivity {
     @Bind(R.id.toolbar_shadow_settings)
     View mToolbarShadow;
 
+    private List<Settings> mSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setupToolbar();
 
+        mSettings = generateSettings();
+
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        SettingsAdapter adapter = new SettingsAdapter(generateSettings());
+        SettingsAdapter adapter = new SettingsAdapter(mSettings);
         mRecyclerView.setAdapter(adapter);
         adapter.setOnItemClickListener((view, position) -> {
-
+            AlertDialog dialog = SettingsAlertDialog
+                    .createDialog(SettingsActivity.this, mSettings.get(position));
+            dialog.show();
         });
     }
 
@@ -56,9 +66,15 @@ public class SettingsActivity extends BaseSignInActivity {
 
     private List<Settings> generateSettings() {
         List<Settings> settingsList = new ArrayList<>();
-        settingsList.add(new Settings("Number of compares", "20"));
-        settingsList.add(new Settings("Language", "English"));
-        settingsList.add(new Settings("Filters", "..."));
+        settingsList.add(new Settings(
+                Resources.getSystem().getString(R.string.dialog_settings_title_compares_count),
+                String.valueOf(Prefs.getNumberOfCompares())));
+        settingsList.add(new Settings(
+                Resources.getSystem().getString(R.string.dialog_settings_title_language),
+                Prefs.getLanguageSettings()));
+//        settingsList.add(new Settings(
+//                Resources.getSystem().getString(R.string.dialog_settings_title_categories),
+//                "..."));
         return settingsList;
     }
 }
