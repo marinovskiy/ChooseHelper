c2e6aaf51d6a04d106516589ee6766d86cd14298
marinovskiy <alexander.marinovskiy@gmail.com>
2016-04-15 22:27:56 +0300

    fix strings.xml

--- app/src/main/java/com/geekhub/choosehelper/utils/Prefs.java
+++ app/src/main/java/com/geekhub/choosehelper/utils/Prefs.java
@@ -16,14 +16,6 @@ public class Prefs {
     public static final int FACEBOOK_LOGIN = 2;
     public static final int FIREBASE_LOGIN = 3;
 
-    // settings
-//    public static final String SETTINGS_LANGUAGE = "settings_language";
-    public static final String SETTINGS_CATEGORIES = "settings_filter_of_compares";
-    public static final String SETTINGS_NUMBER_OF_COMPARES = "settings_number_of_compares";
-    public static final String SETTINGS_NOTIFICATION_LIKED = "settings_notification_liked";
-    public static final String SETTINGS_NOTIFICATION_COMMENTED = "settings_notification_commented";
-    public static final String SETTINGS_NOTIFICATION_NEW_COMPARE = "settings_notification_new_compare";
-
     private static SharedPreferences sPrefs;
 
     private Prefs() {
@@ -50,46 +42,6 @@ public class Prefs {
 
     public static void setUserId(String userId) {
         setString(USER_ID, userId);
-    }
-
-    // settings
-    public static int getNumberOfCompares() {
-        return getInt(SETTINGS_NUMBER_OF_COMPARES);
-    }
-
-    public static void setNumberOfCompares(int numberOfCompares) {
-        setInt(SETTINGS_NUMBER_OF_COMPARES, numberOfCompares);
-    }
-
-    public static Set<String> getCategories() {
-        return getStringSet(SETTINGS_CATEGORIES);
-    }
-
-    public static void setCategories(Set<String> stringSet) {
-        setStringSet(SETTINGS_CATEGORIES, stringSet);
-    }
-
-    public static void setSettingsNotificationLiked(String s) {
-        setString(SETTINGS_NOTIFICATION_LIKED, s);
-    }
-
-    public static String getSettingsNotificationLiked() {
-        return getString(SETTINGS_NOTIFICATION_LIKED);
-    }
-    public static void setSettingsNotificationCommented(String s) {
-        setString(SETTINGS_NOTIFICATION_COMMENTED, s);
-    }
-
-    public static String getSettingsNotificationCommented() {
-        return getString(SETTINGS_NOTIFICATION_COMMENTED);
-    }
-
-    public static void setSettingsNotificationNewCopare(String s) {
-        setString(SETTINGS_NOTIFICATION_NEW_COMPARE, s);
-    }
-
-    public static String getSettingsNotificatioNewCompare() {
-        return getString(SETTINGS_NOTIFICATION_NEW_COMPARE);
     }
 
     // standard methods
